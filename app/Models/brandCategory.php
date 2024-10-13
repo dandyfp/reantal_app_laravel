7f29ff8e28911b344b35b053800d415f2c9f6422
dandyfp <dandyfirmansyah13@gmail.com>
2024-10-13 18:55:36 +0700

    Init project

---
+++ app/Models/brandCategory.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class brandCategory extends Model
+{
+    use HasFactory;
+}
